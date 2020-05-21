bc2559167bb613a4ca6ff68cf95a0c732860a70a
taebinjjang <taebinjjang@naver.com>
2020-05-21 16:56:39 +0900

    connect java to xml file
    (As Known As) connect java to xml file

--- Week02/app/src/main/java/com/example/week02/MainActivity.java
+++ Week02/app/src/main/java/com/example/week02/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.week02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn = (Button) findViewById(R.id.button_1);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(++count+"");
+            }
+        });
     }
 }
